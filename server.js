64966d326f91f4dadbb909569e2f24017d8f4ab8
Rockettube <96451352+Rockettube@users.noreply.github.com>
2021-12-20 21:00:18 -0300

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js')
-const client = new Discord.Client()
-
-client.on("message", message => {
-  if(message.content === "Heil") {
-    return message.channel.send("Lothus")
-  }
-})
-client.login('OTIyNjIzMTUzODU4MDI3NTUx.YcEJxQ.Cz7OGji49suwvcDCe2cMhbPISvQ')
